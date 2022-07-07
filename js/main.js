c67a2461d3a1449a2b156c180cc1b23516a508eb
kareem-4 <official.kysho@gmail.com>
2022-07-07 15:36:38 +0200

    Added Project Files

---
+++ js/main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// navbar variables
+const navbarNav = document.querySelector(".navbar-nav");
+const navbarToggleBtn = document.querySelector(".nav-toggle-btn");
+
+//navbar toggle fucntion
+
+navbarToggleBtn.addEventListener("click" , function() {
+    
+    navbarNav.classList.toggle('active');
+    this.classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+// Loading Screen Execution 
+
+window.addEventListener('load',function() {
+    setTimeout(hideOverlay,2500);
+})
+
+
+
+// Hide Loading Screen Function
+
+function hideOverlay()
+{
+    document.querySelector(".loading-screen").style.display = "none";
+    document.querySelector("body").classList.remove("loading");
+}
+
+
